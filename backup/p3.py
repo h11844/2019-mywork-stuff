132d9caecf8e0e6eacb609bc0e4b0f16e9ac68da
h11844 <h11844@hcjhs.ntpc.edu.tw>
2019-07-29 13:10:02 +0800

    done

--- backup/p3.py
+++ backup/p3.py
@@ -1,9 +1,21 @@
-print("Hello, Python")
-a= 3
-st= "Hello"
-def f(x):
-    return (x**2)
+n= int(input())
+T= {}
+root= list(range(1, n+1))    #所有可能的根節點答案
 
-T={1:[], 2:[6,7], 3:[1,4], 4:[], 5:[3,2], 6:[], 7:[]}
+for i in range(1,n+1):
+    line= input().split()
+    n_line= list(map(int, line))[1:]
+    T[i]= n_line
 
-print(T[3])
+    for k in n_line:
+        root.remove(k)
+
+def h(v):
+    c_nodes=T[v] #子節點
+    if c_nodes==[]:
+        return 0
+    else:
+        return max(map(h,c_nodes))+1
+    
+print(root[0])
+print(sum(map(h, range(1, n+1))))
